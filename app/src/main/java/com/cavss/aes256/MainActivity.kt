a5cecc15d91c40888abf03679872aaee6e3d9c8e
JuYeon Lee <cavss1118@gmail.com>
2022-05-03 17:49:36 +0900

    AES256 암복호화

--- app/src/main/java/com/cavss/aes256/MainActivity.kt
+++ app/src/main/java/com/cavss/aes256/MainActivity.kt
@@ -2,10 +2,19 @@ package com.cavss.aes256
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cavss.aes256.cipher.AESHelper
+import com.cavss.aes256.cipher.AndroidKeyStoreProvider
+import com.cavss.aes256.cipher.ZygoteInit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ZygoteInit.warmUpJcaProviders()
+        AESHelper.keystoreSetting()
+
+        val encryptText = AESHelper.keystoreEncrypt("암호화 할 텍스트".toByteArray(Charsets.UTF_8))
+        val decryptText = String(AESHelper.keystoreDecrypt(encryptText)!!, Charsets.UTF_8)
     }
 }
